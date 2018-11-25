e7e5c59e7466065abb428b5fea738ad298d5d0cb
Thorondo <li_le_cheng@qq.com>
2018-11-25 11:53:31 +0800

    first commit

---
+++ src/md5/Test.java
@@ -0,0 +1,11 @@
+package md5;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="a";
+		System.out.println(MD5.GetMD5Code(a));
+	}
+
+}
